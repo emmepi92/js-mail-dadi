24cbe531be1f849219b9cb167606c846c7ad12cd
emmepi92 <mariapiacoccioli@live.it>
2021-05-31 14:17:19 +0200

    second finished

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ if (check) {
     document.getElementById("result").innerHTML = "La sua email non risulta registrata sulla nostra piattaforma"
 }
 
+// ---------------------------------------------
+
+var computerNumber = Math.floor(Math.random()*6 + 1);
+var userNumber = Math.floor(Math.random()*6 + 1);
+
+var outputHtml = document.getElementById("result-2");
+var result;
+
+if (computerNumber === userNumber) {
+   result = "I due numeri generati sono uguali";
+} else if (computerNumber > userNumber) {
+   result = "Ha vinto il computer!";
+} else {    
+   result = "Ha vinto il giocatore!";
+}
+
+outputHtml.innerHTML = result;
+
+// for easy debug 
+console.log('computer ' + computerNumber);
+console.log('giocatore ' + userNumber);
